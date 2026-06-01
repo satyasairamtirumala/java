2da1fe87e92cff1bf25f9e59c9a38c99a881fa3a
TIRUMALA SATYA SAI RAM <satyasairamtirumala@gmail.com>
2026-06-01 21:32:10 +0530

    Add files via upload

---
+++ HappyNum.java
@@ -0,0 +1,14 @@
+public class HappyNum {
+    public static void main(String[] args) {
+        int n=19;
+        int val=n;
+        int res=0;
+        while(val>0){
+            res+=(val%10)*(val%10);
+            val=val/10;
+            if()
+        }
+        
+       
+    }
+}
