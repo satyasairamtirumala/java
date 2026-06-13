cd304a0cea4ae307bf1376d7f5b01631b47796eb
TIRUMALA SATYA SAI RAM <satyasairamtirumala@gmail.com>
2026-06-13 07:16:58 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,6 @@
+class Details{
+    public static void main(String[] args) {
+        student s1=new student("satya","c001",'m',520,650);
+        s1.Sai1();
+    }
+} 
---
+++ student.java
@@ -0,0 +1,21 @@
+class student{
+    String name;
+    String id;
+    char c;
+    int ten;
+    int twelve;
+    student( String name,String id, char c,int ten,int twelve){
+         this.name=name;
+         this.id=id;
+         this.c=c;
+         this.ten=ten;
+         this.twelve=twelve;
+    }
+    public void Sai1(){
+        System.out.println(this.name);
+        System.out.println(this.id);
+        System.out.println(this.c);
+        System.out.println(this.ten);
+        
+    }
+}
