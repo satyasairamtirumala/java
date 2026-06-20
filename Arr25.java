520ec3a73c60e9741aeb04ba5c810bef5da176ef
TIRUMALA SATYA SAI RAM <satyasairamtirumala@gmail.com>
2026-06-20 07:32:41 +0530

    Add files via upload

---
+++ Arr25.java
@@ -0,0 +1,16 @@
+public class Arr25 {
+    public static void main(String[] args) {
+        int[] arr={10,20,20,30,40,44};
+        for(int i=0;i<arr.length;i++){
+            int count=0;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+           if(count>0){
+            System.out.println(arr[i]);
+           }
+        }
+    }
+}
