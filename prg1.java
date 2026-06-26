6a73412e53d380c7d8b876a50154924c607a3ece
TIRUMALA SATYA SAI RAM <satyasairamtirumala@gmail.com>
2026-06-26 07:10:22 +0530

    Add files via upload

--- prg1.java
+++ prg1.java
@@ -1,9 +1,16 @@
+//write a java prg to find the largest element is 2d array
 public class prg1 {
-    public static void main(String[] args){
-        int i=1;
-        while(i<=5){
-            System.out.println(i);
-            i++;
+    public static void main(String[] args) {
+        int[][] arr={{10,20,30},{40,50,60}
+    };
+    int max=arr[0][0];
+    for(int i=0;i<arr.length;i++){
+        for(int j=0;j<arr[i].length;j++){
+            if(arr[i][j]>max){
+                max=arr[i][j];
+            }
         }
+    }
+    System.out.println(max);
     }
 }
--- prg2.java
+++ prg2.java
@@ -1,9 +1,13 @@
+//prg to find the sum of 2d elements
 public class prg2 {
-    public static void main(String[] args){
-        int i=5;
-        while(i>=1){
-            System.out.println(i);
-            i--;
+    public static void main(String[] args) {
+        int[][] arr={{10,20,30},{40,50,60}};
+    int sum=0;
+    for(int i=0;i<arr.length;i++){
+        for(int j=0;j<arr[i].length;j++){
+            sum+=arr[i][j];
         }
+    }
+    System.out.println(sum);
     }
 }
