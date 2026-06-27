e672565aa165d4d6d38273f2f312c4c4ed6a8482
TIRUMALA SATYA SAI RAM <satyasairamtirumala@gmail.com>
2026-06-27 06:42:32 +0530

    Add files via upload

--- prg4.java
+++ prg4.java
@@ -1,9 +1,16 @@
+//write a java prg to find sum of diagonal elements
 public class prg4 {
     public static void main(String[] args) {
-        int i=2;
-        while(i<=10){
-            System.out.println(i);
-            i+=2;
+        int[][] arr={{10,20,30},{40,50,60},{70,80,90}};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            
+            for(int j=0;j<arr[i].length;j++){
+                if(i==j){
+                    sum+=arr[i][j];
+                }
+            }
         }
+        System.out.println(sum);
     }
 }
--- prg6.java
+++ prg6.java
@@ -1,9 +1,18 @@
+//write a java program to find resultant of (addition)
 public class prg6 {
     public static void main(String[] args) {
-        char c='Z';
-        while(c>='A'){
-            System.out.println(c);
-            c--;
+        int[][] arr={{7,3,5},{2,4,6}};
+        int[][] arr2={{7,3,5},{2,4,6}};
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                arr[i][j]+=arr2[i][j];
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println("");
         }
     }
 }
