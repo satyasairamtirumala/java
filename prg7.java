0278797410dafc4355216d021dddb35efdef8852
TIRUMALA SATYA SAI RAM <satyasairamtirumala@gmail.com>
2026-06-28 07:28:17 +0530

    Add files via upload

--- prg7.java
+++ prg7.java
@@ -1,10 +1,18 @@
+//
 public class prg7 {
     public static void main(String[] args) {
-        char c1='A',c2='z';
-        while(c1<='Z'&& c2>='a'){
-            System.out.println(c1+" "+c2);
-            c1++;
-            c2--;
+        int[][] arr={{3,1},{2,4}};
+        int[][] arr2={{1,3},{4,2}};
+        for(int i=0;i<arr[0].length;i++){
+            for(int j=0;j<arr.length;j++){
+                arr[i][j]+=arr[i][i]*arr2[j][j];
+            }
+        }
+        for(int i=0;i<arr[i].length;i++){
+            for(int j=0;j<arr.length;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println("");
         }
     }
 }
