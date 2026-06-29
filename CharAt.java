e63967fa47c15874d891f2c38ed67828b150ac51
TIRUMALA SATYA SAI RAM <satyasairamtirumala@gmail.com>
2026-06-29 08:39:35 +0530

    Add files via upload

---
+++ CharAt.java
@@ -0,0 +1,8 @@
+public class CharAt{
+    public static void main(String[] args){
+        String name="java";
+        for(int i=0;i<name.length();i++){
+            System.out.println(name.charAt(i));
+        }
+    }
+}
