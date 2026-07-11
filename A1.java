4bb02cc0e21f14330148410e1c715534b9224e0e
TIRUMALA SATYA SAI RAM <satyasairamtirumala@gmail.com>
2026-07-12 05:18:55 +0530

    Add basic array input and output functionality

--- A1.java
+++ A1.java
@@ -1,1 +1,18 @@
 
+
+import java.util.Scanner;
+
+public class A1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int arr[]={1,2,3,4};
+        System.out.println(arr[0]);
+        int arr1[]=new int[4];
+        for(int i=0;i<arr1.length;i++){
+            arr1[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr1.length;i++){
+            System.out.println(arr1[i]);
+        }
+    }
+}
