85d3f6b151489dd5098869ad0d16e8c16b5c84c1
TIRUMALA SATYA SAI RAM <satyasairamtirumala@gmail.com>
2026-07-15 08:46:50 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -1,19 +1,1 @@
-public class prime {
-    public static void main(String[] args) {
-        int n=7;
-        boolean val=true;
-        for(int i=3;i<n;i++){ 
-            if(n%i==0){
-                val=false;
-                break;
-            }
 
-        }
-        if(val==true){
-            System.out.println("Prime");
-        }
-        else{
-            System.out.println("not prime");
-        }
-    }
-}
